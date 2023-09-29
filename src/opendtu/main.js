a33f5971bfdf410b090b36813245c7b970ed4efb
janhuddel <jan.rohwer@gmail.com>
2023-09-29 13:31:56 +0200

    opendtu added

---
+++ src/opendtu/main.js
@@ -0,0 +1,65 @@
+import { config } from "./config.js";
+import { openWebSocket } from "./ws-controller.js";
+import { lineProtocol } from "../common/utils.js";
+
+openWebSocket(config.ws.url, (message) => {
+  try {
+    const payload = JSON.parse(message);
+    const timestamp = new Date();
+    payload.inverters.forEach((inverter) => {
+      // inverter
+      console.log(
+        lineProtocol(
+          config.common.measurement,
+          {
+            vendor: config.common.vendor,
+            friendly: inverter.name,
+            device: inverter.serial,
+          },
+          {
+            power: inverter.AC["0"].Power.v,
+            voltage: inverter.AC["0"].Voltage.v,
+            current: inverter.AC["0"].Current.v,
+            sum_power_total: getPowerInWh(inverter.AC["0"].YieldTotal),
+            sum_power_today: getPowerInWh(inverter.AC["0"].YieldDay),
+          },
+          timestamp
+        )
+      );
+
+      // panels
+      for (const [key, dc] of Object.entries(inverter.DC)) {
+        const device = `${inverter.serial}.${Number(key) + 1}`;
+        const friendly = `Panel#${Number(key) + 1}`;
+        console.log(
+          lineProtocol(
+            config.common.measurement,
+            {
+              vendor: config.common.vendor,
+              friendly,
+              device,
+            },
+            {
+              power: dc.Power.v,
+              voltage: dc.Voltage.v,
+              current: dc.Current.v,
+              sum_power_total: getPowerInWh(dc.YieldTotal),
+              sum_power_today: getPowerInWh(dc.YieldDay),
+            },
+            timestamp
+          )
+        );
+      }
+    });
+  } catch (err) {
+    console.error(`cannot parse payload: ${err.message}`);
+  }
+});
+
+function getPowerInWh(obj) {
+  if (obj.u === "kWh") {
+    return obj.v * 1000.0;
+  } else {
+    return obj.v;
+  }
+}
---
+++ src/opendtu/ws-controller.js
@@ -0,0 +1,79 @@
+import { WebSocket } from "ws";
+import createLogger from "debug";
+
+// inspired by: https://github.com/o0shojo0o/ioBroker.opendtu/blob/main/lib/websocketController.js
+export const openWebSocket = (url, messageHandler) => {
+  const wsHeartbeatIntervall = 10000;
+  const restartTimeout = 5000;
+
+  let ping;
+  let pingTimeout;
+  let autoRestartTimeout;
+
+  let wsClient = null;
+
+  const logger = createLogger("websocket");
+
+  function connect() {
+    logger(`connecting to websocket ${url}...`);
+    wsClient = new WebSocket(url);
+
+    wsClient.on("open", () => {
+      logger("websocket connected");
+
+      // Send ping to server
+      sendPingToServer();
+
+      // Start Heartbeat
+      wsHeartbeat();
+    });
+
+    wsClient.on("pong", () => {
+      logger("got pong from websocket server");
+      wsHeartbeat();
+    });
+
+    wsClient.on("close", () => {
+      logger("websocket closed");
+
+      clearTimeout(pingTimeout);
+      clearTimeout(ping);
+
+      if (wsClient.readyState === WebSocket.CLOSED) {
+        autoRestart();
+      }
+    });
+
+    wsClient.on("error", (err) => {
+      console.error(`Websocket error: ${err}`);
+    });
+
+    wsClient.on("message", messageHandler);
+  }
+
+  function sendPingToServer() {
+    logger("sending ping to server");
+
+    wsClient.ping();
+    ping = setTimeout(() => {
+      sendPingToServer();
+    }, wsHeartbeatIntervall);
+  }
+
+  function wsHeartbeat() {
+    clearTimeout(pingTimeout);
+    pingTimeout = setTimeout(() => {
+      console.error("Websocked timed out");
+      wsClient.terminate();
+    }, wsHeartbeatIntervall + 3000);
+  }
+
+  function autoRestart() {
+    logger(`auto-retry in ${restartTimeout / 1000} seconds...`);
+
+    clearTimeout(autoRestartTimeout);
+    autoRestartTimeout = setTimeout(() => connect(), restartTimeout);
+  }
+
+  connect();
+};
