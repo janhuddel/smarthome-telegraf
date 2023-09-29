ef9a2fef51899f3c9a11317328de684c94e4bd51
Jan Rohwer <jan.rohwer@gmail.com>
2023-09-29 09:14:59 +0200

    initial version

---
+++ common/utils.js
@@ -0,0 +1,34 @@
+export function lineProtocol(measurement, tags, fields, timestamp) {
+  let line = `${measurement}`;
+
+  // Tags
+  if (tags) {
+    line +=
+      "," +
+      Object.entries(tags)
+        .map(([key, value]) => `${key}=${value}`)
+        .join(",");
+  }
+
+  // Fields
+  line +=
+    " " +
+    Object.entries(fields)
+      .map(([key, value]) => {
+        if (typeof value === "string" || value instanceof String) {
+          return `${key}="${value}"`;
+        } else {
+          return `${key}=${value}`;
+        }
+      })
+      .join(",");
+
+  // Timestamp
+  if (timestamp instanceof Date) {
+    line += ` ${timestamp.getTime() * 1000000}`;
+  } else {
+    line += ` ${timestamp}`;
+  }
+
+  return line;
+}
---
+++ tasmota/main.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+
+import { connectAsync } from "mqtt";
+import { config } from "./config.js";
+import { lineProtocol } from "../common/utils.js";
+
+async function main() {
+  const mqttClient = await connectAsync(
+    `mqtt://${config.mqtt.broker}`,
+    config.mqtt.connectOptions
+  );
+
+  await mqttClient.subscribeAsync(["tele/+/SENSOR"], { qos: config.mqtt.qos });
+
+  mqttClient.on("message", async (topic, message) => {
+    const topicParts = topic.split("/");
+    const deviceId = topicParts[1];
+    const device = config.devices.find((d) => d.id === deviceId);
+
+    //const device = config.devices[deviceId];
+
+    if (device) {
+      const sensorData = JSON.parse(message);
+      try {
+        await processSensorData(device, sensorData);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+  });
+}
+
+async function processSensorData(device, sensorData) {
+  const power = sensorData.ENERGY.Power;
+  const time = new Date(sensorData.Time);
+
+  if (!Array.isArray(power)) {
+    // Single-Channel
+    console.log(
+      lineProtocol(
+        config.common.measurement,
+        {
+          vendor: config.common.vendor,
+          device: device.id,
+          friendly: device.channels[0],
+        },
+        {
+          power,
+          voltage: sensorData.ENERGY.Voltage,
+          current: sensorData.ENERGY.Current,
+          sum_power_today: sensorData.ENERGY.Today * 1000.0,
+          sum_power_total: sensorData.ENERGY.Total * 1000.0,
+        },
+        time
+      )
+    );
+  } else {
+    // Multi-Channel (die Totals müssen per Http nachgelesen werden, da sie nicht in den Sensordaten enthalten sind)
+    const response = await axios.get(`http://${device.ip}/cm?cmnd=EnergyTotal`);
+    const energyTotals = response.data.EnergyTotal;
+
+    for (const [i, channel] of device.channels.entries()) {
+      console.log(
+        lineProtocol(
+          config.common.measurement,
+          {
+            vendor: config.common.vendor,
+            device: `${device.id}.${i}`,
+            friendly: channel,
+          },
+          {
+            power: power[i],
+            voltage: sensorData.ENERGY.Voltage,
+            current: sensorData.ENERGY.Current[i],
+            sum_power_today: energyTotals.Today[i] * 1000.0,
+            sum_power_total: energyTotals.Total[i] * 1000.0,
+          },
+          time
+        )
+      );
+    }
+  }
+}
+
+main();
